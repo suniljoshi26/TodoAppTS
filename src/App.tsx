7c4fb9edf35cb1485509ede31bd70edccab4b1d2
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-30 18:50:02 +0530

    Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import H1 from "./H1";
 import H3 from "./h3";
+import Input from "./Input";
 import { NavBar } from "./NavBar";
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       {" "}
       <NavBar />
       <H1 title=" Things to get done"></H1>
-      <H3 title="ettwertw"></H3>
+      <H3 title="Things to do"></H3>
+      <Input />
     </div>
   );
 }
---
+++ src/Input.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <input
+        className="p-2 border border-gray-300   w-44 sm:w-72  rounded-md "
+        placeholder="Write an artical about XState"
+      />
+    </div>
+  );
+};
+
+export default Input;
