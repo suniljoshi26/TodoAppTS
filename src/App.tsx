d90610a0d27a2e08560d6ab1840180c58b969bf9
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-30 20:43:40 +0530

    Button comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Button from "./Button";
 import H1 from "./H1";
 import H3 from "./h3";
 import Input from "./Input";
@@ -14,6 +15,7 @@ function App() {
       <H1 title=" Things to get done"></H1>
       <H3 title="Things to do"></H3>
       <Input />
+      <Button>save</Button>
     </div>
   );
 }
---
+++ src/Button.tsx
@@ -0,0 +1,35 @@
+import React, { ButtonHTMLAttributes, FC } from "react";
+
+type ButtonProps = {
+  them?: "highlight" | "secondary" | "primary";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = ({ them, className, ...rest }) => {
+  console.log(them);
+  let themClass =
+    " text-white rounded-md bg-yellow-500 border-transparent  hover:bg-yellow-600 ";
+  let radiusClass = "rounded-md";
+  if (them === "highlight") {
+    radiusClass = " mt-2 rounded-full";
+  }
+  if (them === "secondary") {
+    themClass = "text-gray-900 bg-white  border-gray-300 hover:bg-gray-600";
+  }
+
+  return (
+    <button
+      className={
+        "px-4 py-2 text-sm font-medium  border  shadow-sm disabled:bg-gray-400 " +
+        className +
+        themClass +
+        radiusClass
+      }
+      {...rest}
+    ></button>
+  );
+};
+Button.defaultProps = {
+  them: "primary",
+};
+
+export default Button;
