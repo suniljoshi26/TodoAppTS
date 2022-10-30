7821e7ff10ad8c42add59921dfa71f853b0fbc36
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-30 16:56:05 +0530

    h1 com

--- src/H1.tsx
+++ src/H1.tsx
@@ -3,7 +3,7 @@ type headingProps = {
   title: string;
 };
 const H1: FC<headingProps> = ({ title }) => {
-  return <h3 className=" text-4xl font-bold mt-32">{title}</h3>;
+  return <h1 className=" text-4xl font-bold mt-32">{title}</h1>;
 };
 
 export default H1;
