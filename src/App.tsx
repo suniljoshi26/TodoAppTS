5764cb53b1ff42d24c9a33bd28b5dd87a08f13bf
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-31 14:35:04 +0530

    todo form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import Button from "./Button";
 import H1 from "./H1";
-import H3 from "./h3";
+import H3 from "./H3";
 import Input from "./Input";
 import { NavBar } from "./NavBar";
+import TodoForn from "./TodoForn";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,8 +15,7 @@ function App() {
       <NavBar />
       <H1 title=" Things to get done"></H1>
       <H3 title="Things to do"></H3>
-      <Input />
-      <Button>save</Button>
+      <TodoForn />
     </div>
   );
 }
---
+++ src/TodoForn.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "./Button";
+import H3 from "./H3";
+import Input from "./Input";
+
+const TodoForn = () => {
+  return (
+    <div className="border border-gray-200 p-4 ">
+      <H3 title="Create a todo" />
+      <div className="mt-4">
+        <Input />
+      </div>
+      <div className="space-x-6 mt-4 ">
+        <Button>Save</Button>
+        <Button them="secondary">Cancle</Button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoForn;
