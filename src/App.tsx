d8e758cc9bc3c680a0aff4361ef9561b3debd99a
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-02 13:05:50 +0530

    code sift todo page

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,11 @@
-import { FC, useState } from "react";
-import Button from "./Button";
-import H1 from "./H1";
-import H3 from "./H3";
+import React from "react";
 
-import { ITodo } from "./interfaces";
-import Msg from "./Msg";
-import { NavBar } from "./NavBar";
-import TodoForn from "./TodoForn";
-import TodoRow from "./TodoRow";
+import TodoPage from "./TodoPage";
 
-const App: FC = () => {
-  const a = JSON.parse(localStorage.getItem("todoList") || "[]");
-  const b = JSON.parse(localStorage.getItem("doneList") || "[]");
-  const [formVisable, setFormVisable] = useState(false);
-  const [todoList, setTodoList] = useState<ITodo[]>(a);
-  const [doneList, setDoneList] = useState<ITodo[]>(b);
-  localStorage.setItem("todoList", JSON.stringify(todoList));
-  localStorage.setItem("doneList", JSON.stringify(doneList));
-
-  console.log("todo", todoList);
-
-  const addTodo = (todo: any) => {
-    const t = [...todoList, todo];
-    console.log("yty", t);
-    setTodoList(t);
-  };
-  const handleShowForm = () => {
-    setFormVisable(true);
-  };
-  const handleHideForm = () => {
-    setFormVisable(false);
-  };
-  const markAsNotDone = (todo: ITodo) => {
-    const newDone = doneList.filter((task) => task !== todo);
-
-    setDoneList(newDone);
-    setTodoList([...todoList, todo]);
-  };
-  const markAsDone = (todo: ITodo) => {
-    const newTodo = todoList.filter((task) => task !== todo);
-
-    console.log("new todo", newTodo);
-    setTodoList(newTodo);
-
-    setDoneList([...doneList, todo]);
-    console.log("sdasds", newTodo.length);
-  };
-  const todoDelete = (todo: ITodo, done: boolean) => {
-    console.log("delete", todo);
-    if (done) {
-      const newDoneList = doneList.filter((task) => task !== todo);
-      setDoneList(newDoneList);
-    } else {
-      const newTodoList = todoList.filter((task) => task !== todo);
-      setTodoList(newTodoList);
-    }
-  };
-
+const App = () => {
   return (
     <div className=" ">
-      {" "}
-      <NavBar todoList={todoList} doneList={doneList} />
-      <div className=" px-10 sm:px-32">
-        <H1 title=" Things to get done"></H1>
-        <H3 title="Things to do"></H3>
-        {todoList.length === 0 && <Msg />}
-        {todoList.map((todo, key: number) => (
-          <TodoRow
-            key={key}
-            todo={todo}
-            done={false}
-            onStatusChange={markAsDone}
-            onDelete={todoDelete}
-          />
-        ))}
-        {!formVisable && (
-          <Button them="highlight" onClick={handleShowForm}>
-            + Add todo
-          </Button>
-        )}
-        {formVisable && (
-          <TodoForn handleHideForm={handleHideForm} addTodo={addTodo} />
-        )}
-        <H3 title="Things to done"></H3>
-        {doneList.length === 0 && <Msg />}
-        {doneList.map((todo: ITodo, key: number) => (
-          <TodoRow
-            key={key}
-            todo={todo}
-            done={true}
-            onStatusChange={markAsNotDone}
-            onDelete={todoDelete}
-          />
-        ))}
-      </div>
+      <TodoPagez />
     </div>
   );
 };
